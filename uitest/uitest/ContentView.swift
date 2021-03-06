d621d920ae802ad5c70cf5c22a53851d58a3bb9d
bink <eng116dtu@gmail.com>
2021-03-06 11:22:56 +0700

    init

---
+++ uitest/uitest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Binh Nguyen on 04/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
